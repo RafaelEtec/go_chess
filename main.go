ca91f7b58f381b14c97512cb513d8c4804f11029
Rafael Goulart <rafaelxms@hotmail.com>
2024-12-22 15:28:53 -0300

    mechanics thinking

--- main.go
+++ main.go
@@ -6,7 +6,52 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
-type Game struct{}
+const (
+	SCREEN_WIDTH  = 440
+	SCREEN_HEIGHT = 400
+
+	BOARD_WIDTH  = SCREEN_WIDTH - 40
+	BOARD_HEIGHT = SCREEN_HEIGHT
+
+	FRAME_OX    = 0
+	FRAME_OY    = 0
+	FRAME_WIDTH = 32
+
+	COLS = 8
+	ROWS = 8
+
+	MESSAGE_WHOPLAYS = "Player %d turn"
+)
+
+type Game struct {
+	state   int
+	message string
+
+	whoplay int
+
+	board *Board
+}
+
+type Board struct {
+	Pieces [][]*Piece
+}
+
+type Piece interface {
+	Img() *ebiten.Image
+
+	Color() int
+
+	X() int
+	Y() int
+
+	C() int
+	R() int
+	PC() int
+	PR() int
+}
+
+type Pawn struct {
+}
 
 func (g *Game) Update() error {
 	return nil
