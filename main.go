4ed5aef3d0bfe7485eb1bd0069a24ba9c528773b
Rafael Goulart <rafaelxms@hotmail.com>
2024-12-23 13:53:03 -0300

    board presenting

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"image"
+	"image/color"
 	"log"
 
+	"github.com/hajimehoshi/ebiten/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
@@ -13,8 +16,6 @@ const (
 	BOARD_WIDTH  = SCREEN_WIDTH - 40
 	BOARD_HEIGHT = SCREEN_HEIGHT
 
-	FRAME_OX    = 0
-	FRAME_OY    = 0
 	FRAME_WIDTH = 32
 
 	COLS = 8
@@ -33,7 +34,7 @@ type Game struct {
 }
 
 type Board struct {
-	Pieces [][]*Piece
+	Pieces []*Piece
 }
 
 type Piece interface {
@@ -50,25 +51,76 @@ type Piece interface {
 	PR() int
 }
 
-type Pawn struct {
-}
+type Blank struct{}
+
+type Pawn struct{}
+
+type Knight struct{}
+
+type Bishop struct{}
+
+type Queen struct{}
+
+type King struct{}
 
 func (g *Game) Update() error {
+
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
+	screen.Fill(color.RGBA{0, 0, 0, 255})
+	opts := ebiten.DrawImageOptions{}
 
+	drawBoard(g, opts, screen)
 }
 
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+func drawBoard(g *Game, opts ebiten.DrawImageOptions, screen *ebiten.Image) {
+	light_tile, err := ebitenutil.NewImageFromURL("assets/board/light_tile.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dark_tile, err := ebitenutil.NewImageFromURL("assets/board/dark_tile.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tile := light_tile
+	for i := 0; i < COLS; i++ {
+		color := 0
+		for j := 0; j < ROWS; j++ {
+			if color%2 == 0 {
+				tile = light_tile
+				color = 2
+			} else {
+				tile = dark_tile
+				color = 0
+			}
+
+			opts.GeoM.Translate(float64(i)*FRAME_WIDTH, float64(j)*FRAME_WIDTH)
+			screen.DrawImage(
+				tile.SubImage(
+					image.Rect(32, 32, 32, 32),
+				).(*ebiten.Image),
+				&opts,
+			)
+
+			opts.GeoM.Reset()
+		}
+	}
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return SCREEN_WIDTH, SCREEN_HEIGHT
 }
 
 func main() {
-	ebiten.SetWindowSize(640, 480)
+	g := &Game{}
+
+	ebiten.SetWindowSize(SCREEN_WIDTH*2, SCREEN_HEIGHT*2)
 	ebiten.SetWindowTitle("CHESS by Rafael Goulart")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	if err := ebiten.RunGame(g); err != nil {
 		log.Fatal(err)
 	}
 }
