3009218807c0a3102abf2c29f8bd4e031cb2e983
Rafael Goulart <rafaelxms@hotmail.com>
2024-12-23 14:22:50 -0300

    added pieces

--- main.go
+++ main.go
@@ -13,8 +13,8 @@ const (
 	SCREEN_WIDTH  = 440
 	SCREEN_HEIGHT = 400
 
-	BOARD_WIDTH  = SCREEN_WIDTH - 40
-	BOARD_HEIGHT = SCREEN_HEIGHT
+	BOARD_WIDTH  = 400
+	BOARD_HEIGHT = 400
 
 	FRAME_WIDTH = 32
 
@@ -37,34 +37,21 @@ type Board struct {
 	Pieces []*Piece
 }
 
-type Piece interface {
-	Img() *ebiten.Image
+type Piece struct {
+	Img *ebiten.Image
 
-	Color() int
+	Color int
 
-	X() int
-	Y() int
+	X int
+	Y int
 
-	C() int
-	R() int
-	PC() int
-	PR() int
+	C  int
+	R  int
+	PC int
+	PR int
 }
 
-type Blank struct{}
-
-type Pawn struct{}
-
-type Knight struct{}
-
-type Bishop struct{}
-
-type Queen struct{}
-
-type King struct{}
-
 func (g *Game) Update() error {
-
 	return nil
 }
 
@@ -72,46 +59,30 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	screen.Fill(color.RGBA{0, 0, 0, 255})
 	opts := ebiten.DrawImageOptions{}
 
-	drawBoard(g, opts, screen)
+	drawBoard(opts, screen)
 }
 
-func drawBoard(g *Game, opts ebiten.DrawImageOptions, screen *ebiten.Image) {
-	light_tile, err := ebitenutil.NewImageFromURL("assets/board/light_tile.png")
+func drawBoard(opts ebiten.DrawImageOptions, screen *ebiten.Image) {
+	light_tile, err := ebitenutil.NewImageFromURL("https://github.com/RafaelEtec/go_chess/blob/4ed5aef3d0bfe7485eb1bd0069a24ba9c528773b/assets/board/light_tile.png?raw=true")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	dark_tile, err := ebitenutil.NewImageFromURL("assets/board/dark_tile.png")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	tile := light_tile
 	for i := 0; i < COLS; i++ {
-		color := 0
 		for j := 0; j < ROWS; j++ {
-			if color%2 == 0 {
-				tile = light_tile
-				color = 2
-			} else {
-				tile = dark_tile
-				color = 0
-			}
-
 			opts.GeoM.Translate(float64(i)*FRAME_WIDTH, float64(j)*FRAME_WIDTH)
 			screen.DrawImage(
-				tile.SubImage(
-					image.Rect(32, 32, 32, 32),
+				light_tile.SubImage(
+					image.Rect(32, 32, 0, 0),
 				).(*ebiten.Image),
 				&opts,
 			)
-
 			opts.GeoM.Reset()
 		}
 	}
 }
 
-func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
 	return SCREEN_WIDTH, SCREEN_HEIGHT
 }
 
